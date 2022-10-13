b7b47889349248be1b1595db89f4d3e71a2984a8
alugocp <alugocp@aim.com>
2022-10-13 19:17:27 -0400

    Using generated theme

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Alex's Emacs setup
 Yep, I'm an Emacs user now.
+
+## Getting started
+Make sure you delete your `~/.emacs` file or Emacs won't look for `~/.emacs.d/init.el`.
+Run the following command to make this starter kit usable:
+
+```sh
+cp init.el.template init.el
+```
--- init.el
+++ init.el
@@ -1,19 +1,19 @@
-(add-to-list 'load-path "~/.emacs.d/color-theme-sanityinc-solarized")
-(require 'color-theme-sanityinc-solarized)
-(defun reapply-themes ()
-  (load-theme 'sanityinc-solarized-dark))
-(add-hook 'after-init-hook 'reapply-themes)
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" default)))
+   '("6678ff97b5a734075093a5eb9694bfef55090bc7bcd412943a9f05e869ba9392" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+ (add-to-list 'load-path "~/.emacs.d/lisp")
+ (require 'basic-theme)
+ (defun reapply-themes ()
+   (load-theme 'basic))
+ (add-hook 'after-init-hook 'reapply-themes)
