01730cd3ee6281cb4c9551f88526c9cbbe493d02
alugocp <alugocp@aim.com>
2022-10-13 22:31:21 -0400

    Set some appearances

--- init.el
+++ init.el
@@ -6,7 +6,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("6678ff97b5a734075093a5eb9694bfef55090bc7bcd412943a9f05e869ba9392" default))
+   '("0193c7fd759cf243912d689a196777f338a92a1af3aae75041726c8835e1c840" "8761f71a95a63a1d0e321ea9e32e772fe4041def2a5233d70e4f5c25ae9edbd9" "6678ff97b5a734075093a5eb9694bfef55090bc7bcd412943a9f05e869ba9392" default))
  '(package-selected-packages '(typescript-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
--- lisp/startup.el
+++ lisp/startup.el
@@ -1,11 +1,23 @@
-;; General setup
+;; Miscellaneous variables
 (setq next-line-add-newlines nil)
 (setq backup-inhibited t)
 (setq auto-save-default nil)
 (setq create-lockfiles nil)
-(global-display-line-numbers-mode)
 (setq inhibit-startup-message t)
+(setq transient-mark-mode  (cons 'only transient-mark-mode))
+
+;; Modal function calls
+(global-display-line-numbers-mode)
 (tool-bar-mode -1)
+(set-frame-size (selected-frame) 150 50)
+(set-face-attribute 'default nil :height 150)
+
+;; Set the UI style
+;; https://emacsfodder.github.io/emacs-theme-editor/
+(require 'billw-theme)
+(defun reapply-themes ()
+  (load-theme 'billw))
+(add-hook 'after-init-hook 'reapply-themes)
 
 ;; Use the MELPA package archive
 (require 'package)
@@ -22,11 +34,21 @@
   (unless (package-installed-p pkg)
     (package-install pkg)))
 
-;; Set the UI style
-;; https://emacsfodder.github.io/emacs-theme-editor/
-(require 'basic-theme)
-(defun reapply-themes ()
-  (load-theme 'basic))
-(add-hook 'after-init-hook 'reapply-themes)
+;; Custom key binding functions
+(defun highlight-line ()
+  "Select the current line"
+  (interactive)
+  (move-beginning-of-line 1)
+  (push-mark nil nil t)
+  (forward-line 1)
+)
 
+;; Key bindings
+(global-set-key (kbd "s-<left>") 'move-beginning-of-line)
+(global-set-key (kbd "s-<right>") 'move-end-of-line)
+(global-set-key (kbd "s-<up>") 'beginning-of-buffer)
+(global-set-key (kbd "s-<down>") 'end-of-buffer)
+(global-set-key (kbd "s-l") 'highlight-line)
+
+;; Okay we're done now
 (provide 'startup)
